f33c3b4757de4087832ccbd83514dcb1e7f445aa
Tulika Harsulkar <tharsulkar092@gmail.com>
2016-03-13 07:15:08 -0400

    Added Underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 5000;
@@ -12,19 +13,21 @@ app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
 
+//GET/todos
 app.get('/todos', function (req,res){
 	res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function (todo){
-		if(todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach(function (todo){
+	// 	if(todoId === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -33,10 +36,15 @@ app.get('/todos/:id', function (req, res){
 	}
 });
 
-
+	//POST /todos
 	app.post('/todos', function (req, res){
-		var body = req.body;
+		var body = _.pick(req.body, 'description', 'completed');
 
+		if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+			return res.status(400).send();
+		}
+
+		body.description = body.description.trim();
 		body.id = todoNextId++;
 		todos.push(body);
 		
