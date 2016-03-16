b4bba908a7173305633ab55791e1251d9440c0ba
Tulika Harsulkar <tharsulkar092@gmail.com>
2016-03-15 20:11:12 -0400

    add todo model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js')
 
 var app = express();
 var PORT = process.env.PORT || 5000;
@@ -13,7 +14,7 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-//GET/todos?completed=true
+// GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -30,25 +31,19 @@ app.get('/todos', function(req, res) {
 
 	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
 		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1;
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
 	}
 
 	res.json(filteredTodos);
 });
 
-//GET /todos/:id
+// GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {
 		id: todoId
 	});
-
-	// todos.forEach(function (todo){
-	// 	if(todoId === todo.id) {
-	// 		matchedTodo = todo;
-	// 	}
-	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -57,22 +52,32 @@ app.get('/todos/:id', function(req, res) {
 	}
 });
 
-//POST /todos
+// POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+	// call create on db.todo
+	//   respond with 200 and todo
+	//   res.status(400).json(e)
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
-	todos.push(body);
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	res.json(body);
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
+
+	// todos.push(body);
+
+	// res.json(body);
 });
 
-//DELETE /todos/:id
+// DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {
@@ -80,43 +85,46 @@ app.delete('/todos/:id', function(req, res) {
 	});
 
 	if (!matchedTodo) {
-		res.status(404).send();
+		res.status(404).json({
+			"error": "no todo found with that id"
+		});
 	} else {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
 });
 
-//PUT /todos/:id
+// PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
-	var body = _.pick(req.body, 'description', 'completed');
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {
 		id: todoId
 	});
-	var validAttributes = {};
 	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
 
 	if (!matchedTodo) {
 		return res.status(404).send();
 	}
 
-
-
 	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed'))
+	} else if (body.hasOwnProperty('completed')) {
 		return res.status(400).send();
+	}
 
 	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description.trim();
+		validAttributes.description = body.description;
 	} else if (body.hasOwnProperty('description')) {
 		return res.status(400).send();
 	}
+
 	_.extend(matchedTodo, validAttributes);
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + ' !');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
